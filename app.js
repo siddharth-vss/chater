5dc6bc49b6cdca0f27cc3c351385bb1ac5330652
Siddharth <siddharth@vasantsoftwaressolution.com>
2023-11-07 15:41:45 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 const dotenv = require('dotenv');
-
+ 
 const cors = require('cors')
 
 // const indexRouter = require('./routes/index');
@@ -16,8 +16,8 @@ const messagesRouter = require('./routes/messages');
 const app = express();
 const PORT = 5000;
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
+
 
 dotenv.config();
 
@@ -25,21 +25,38 @@ dotenv.config();
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chats', chatsRouter);
 app.use('/messages', messagesRouter);
 
-app.get('/',(req,res)=>{
-  res.render('index',{title:"Express"});
-})
 
+/**
+|--------------------------------------------------|
+|                   Deployment                     |
+|--------------------------------------------------|
+*/
 
+// const __dirname= path.resolve();
 
+if(process.env.NODE_ENV === "production"){
+   
+  app.use(express.static(path.join(__dirname,'/client/build')));
+   
+  app.get('*',(req,res)=>{
+     res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+  })
+}else{
+  app.get('/',(req,res)=>{
+    res.send("API in running ");
+  })
+}
+
+//|------------------------------------------------|
 const server = app.listen(PORT,()=>{
-  log(`server running on http://localhost:${PORT}`.rainbow.bold);
+  console.log(`server running on http://localhost:${PORT}`.rainbow.bold);
 });
 
 const io = require("socket.io")(server, {
